c8be297a27a4ead884e49abf236753bd2504cc88
mattig94 <mattig94@users.noreply.github.com>
2019-07-18 22:44:28 -0400

    updated all current content utilizing Bootstrap

--- js/script.js
+++ js/script.js
@@ -14,12 +14,10 @@ var pokemonRepository = (function() {
 
 	function addListItem(pokemon) {
 		var $pokemonList = $('.pokemon-list');
-		var listItem = $('<li></li>');
 		var nameButton = $(
-			'<button class="main-pokemon-button">' + pokemon.name + '</button>'
+			'<button type="button" class="main-pokemon-button btn btn-info list-group-item list-group-item-action text-capitalize" data-toggle="modal" data-target="#pokemon-details">' + pokemon.name + '</button>'
 		);
-		listItem.append(nameButton);
-		$pokemonList.append(listItem);
+		$pokemonList.append(nameButton);
 		//open modal
 		nameButton.click(function() {
 			showDetails(pokemon);
@@ -29,8 +27,6 @@ var pokemonRepository = (function() {
 	function showDetails(item) {
 		pokemonRepository.loadDetails(item).then(function() {
 			createDetails(item);
-			//show the modal
-			$('#modal-container').addClass('is-visible');
 		});
 	}
 
@@ -57,6 +53,7 @@ var pokemonRepository = (function() {
 				//add details to the item
 				item.imageUrl = details.sprites.front_default;
 				item.height = details.height;
+				item.weight = details.weight;
 				item.types = Object.keys(details.types);
 			})
 			.catch(function(e) {
@@ -67,46 +64,27 @@ var pokemonRepository = (function() {
 	//create modal
 	function createDetails(item) {
 		//variables
-		var $modalContainer = $('#modal-container');
-		var modal = $('<div class="modal"></div>');
-		var closeButton = $('<button class="modal-close">close</button>');
+		var $modalBody = $('.modal-body');
+		var modal = $('<div></div>');
 		var imgElement = $(
 			'<img src="' + item.imageUrl + '" alt="Picture of ' + item.name + '">'
 		);
-		var nameTitle = $('<h2>' + item.name + '</h2>');
+		var nameTitle = $('<h2 class="text-capitalize">' + item.name + '</h2>');
 		var heightElement = $('<p>Height: ' + item.height + '</p>');
+		var weightElement = $('<p>Weight: ' + item.weight + '</p>');
 		//var typeElement = $('<p></p>');
 
 		//clear contents
-		$modalContainer.empty();
+		$modalBody.empty();
 
 		//add contents
-		modal.append(closeButton);
 		modal.append(imgElement);
 		modal.append(nameTitle);
 		modal.append(heightElement);
+		modal.append(weightElement);
 		//modal.append(typeElement);
-		$modalContainer.append(modal);
+		$modalBody.append(modal);
 	}
-
-	//basic function to hide modal
-	function hideModal() {
-		$('#modal-container').removeClass('is-visible');
-	}
-
-	//closing modal
-	$('body').click(e => {
-		var target = $(e.target);
-		if (target.is($('.modal-close'))) {
-			hideModal();
-		}
-	});
-
-	$('body').keydown(e => {
-		if (e.which === 27 && $('#modal-container').hasClass('is-visible')) {
-			hideModal();
-		}
-	});
 
 	return {
 		add: add,
